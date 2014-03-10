a306e9306e48bbbaef5d7914e861af7296183afe
Ronny Wang <ronnywang@gmail.com>
2014-03-10 21:47:47 +0800

    pic2linesjson 改成 threshold, minLineLength, maxLineGap 可以自帶參數

--- scripts/pic2linesjson.cpp
+++ scripts/pic2linesjson.cpp
@@ -4,11 +4,26 @@
 
 int main(int argc, char** argv)
 {
+    int threshold = 80;
+    double minLineLength = 400;
+    double maxLineGap = 10;
+    if (argc > 2) {
+        threshold = atoi(argv[2]);
+    }
+    if (argc > 3) {
+        minLineLength = atof(argv[3]);
+    }
+    if (argc > 4) {
+        maxLineGap = atof(argv[4]);
+    }
+
+
     cv::Mat src1;
     src1 = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-    cv::Mat img;
+    cv::Mat img, output;
     cvtColor(src1, img, CV_BGR2GRAY);
+    output = src1;
     cv::Size size(3, 3);
     cv::GaussianBlur(img, img, size, 0);
     adaptiveThreshold(img, img, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 75, 10);
@@ -16,7 +31,7 @@ int main(int argc, char** argv)
 
     cv::vector<cv::Vec4i> lines;
 
-    HoughLinesP(img, lines, 1, CV_PI / 180, 80, 400, 10);
+    HoughLinesP(img, lines, 1, CV_PI / 180, threshold, minLineLength, maxLineGap);
     cv::Size img_size;
     printf("{\"size\":[%d,%d],\"lines\":", img.size().width, img.size().height);
 
@@ -29,8 +44,10 @@ int main(int argc, char** argv)
         } else {
             printf(",");
         }
+        cv::line(output, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 0, 255), 1);
         printf("[%d,%d,%d,%d]", l[0], l[1], l[2], l[3]);
     }
+    cv::imwrite("opencv.png", output);
     printf("]}\n");
 
     return 0;
