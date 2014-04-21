173edbc25c6fc4d772e0e2b5615397b54d69b9e3
Shenk Wang <shenk@polynomial.(none)>
2014-04-22 00:08:38 +0800

    merge mutiple convert command into one to reduce unnecessary IO

--- scripts/removeWatermark.sh
+++ scripts/removeWatermark.sh
@@ -1,4 +1,5 @@
 input="origin.png" #要被處理的檔案
+output="result.png"
 mask="../mask.png" #遮罩檔
 resolution=`identify "$input" |cut -d' ' -f3`
 
@@ -10,7 +11,10 @@ y2=775
 
 degree=`echo "a(($y2-$y1)/($x2-$x1))*45/a(1)"|bc -l`
 
-convert "$mask" -extent $resolution -filter point -distort SRT "0,0 1 $degree $x1,$y1" -negate region.png #製作遮罩
-convert "$input" -alpha on \( +clone -channel a -fx 0 \) +swap region.png -composite focus.png #取出該被特別關照的區塊
-convert focus.png -morphology Close Octagon:2 denoised.png #去除汙點 (浮水印)
-convert "$input" denoised.png -composite result.png #疊回去
+convert "$input" \( \
+    +clone \
+    \( "$mask" -extent $resolution -filter point -distort SRT "0,0 1 $degree $x1,$y1" -negate \) \
+    -compose CopyOpacity \
+    -composite \
+    -morphology Close Octagon:2 \
+\) -compose Over -composite -monochrome "$output"
